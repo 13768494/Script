9576b3d03523c3533ba706d1dee20bfda0b6990c
帅成马 <90511611+13768494@users.noreply.github.com>
2025-06-10 00:15:38 +0800

    Add files via upload

---
+++ 句首排序.py
@@ -0,0 +1,15 @@
+with open("text.txt", "r", encoding="utf-8") as f:
+    text = f.readlines()
+f.close()
+
+line = []
+for i in range(len(text)):
+    line.append(text[i].strip())
+
+line = list(set(line))
+line = sorted(line)
+
+with open("result.txt", "w", encoding="utf-8") as f:
+    for i in range(len(line)):
+        f.write(line[i] + "\n")
+f.close()
