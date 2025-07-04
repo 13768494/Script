f0f19892f8090a5358c0956b0dde31b95a1b7f57
帅成马 <90511611+13768494@users.noreply.github.com>
2025-07-04 13:50:54 +0800

    Update 文件捕获.py

--- 文件捕获.py
+++ 文件捕获.py
@@ -4,6 +4,7 @@ from win32api import GetLogicalDriveStrings
 from win32file import *
 
 U = GetLogicalDriveStrings()
+# 数据清洗
 U_list = U.split('\x00')
 U_list.pop(-1)
 
@@ -17,7 +18,7 @@ for root,ds,fs in os.walk(UP):  # 该盘符内所有目标文件都可以获取
     for files in fs:
         if ".txt" in files:    # 目标文件类型
             file_path = root + "\\" + files
-            save_path = "C:\\Users\\123\Desktop\\1\\新建文件夹\\" + files
+            save_path = "C:\\……\\" + files
             CopyFile(file_path, save_path ,False)
             count += 1
 print(f"已复制{count}个文件")
