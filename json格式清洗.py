77b8f8ed93976605043b3734aa484c09afa32152
帅成马 <90511611+13768494@users.noreply.github.com>
2026-01-07 17:46:48 +0800

    Add files via upload

---
+++ json格式清洗.py
@@ -0,0 +1,19 @@
+with open("./data.txt",'r',encoding="utf-8") as f:
+    lines = f.read()
+    with open("./results.txt",'w',encoding="utf-8") as ff:
+        for i in range(len(lines)):
+            if lines[i] == ',':
+                print(f"{lines[i]}")
+                ff.write('\n')
+            elif lines[i] == '}':
+                ff.write('\n' + '=============================')
+            elif lines[i] != '{':
+                print(f"{lines[i]}",end="")
+                ff.write(lines[i])
+    ff.close()
+f.close()
+
+print("\n")
+print("=============================\n")
+print("=============OK==============\n")
+print("=============================\n")
